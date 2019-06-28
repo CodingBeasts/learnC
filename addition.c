eae50a6a942750bc056785493ceb9f38c97ee738
DipanshuShora <deepanshushora@gmail.com>
2019-06-29 04:24:42 +0530

    Variables And Expressions

---
+++ addition.c
@@ -0,0 +1,16 @@
+// taking input of two number and print their sum
+#include<stdio.h>
+
+int main(){
+    int num1,num2,sum;
+
+    printf("Enter number 1 : ");
+    scanf("%d",&num1);
+
+    printf("\nEnter numner 2 : ");
+    scanf("%d",&num2);
+
+    sum=num1+num2;
+    printf("Sum of %d and %d = %d\n",num1,num2,sum);
+
+}
---
+++ reply.c
@@ -0,0 +1,9 @@
+//print something on screen
+#include<stdio.h>
+
+int main(){
+  printf("How are you? \n");
+  printf("I am fine. \n");
+
+  return 0;
+}
