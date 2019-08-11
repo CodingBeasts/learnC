a0014c781b09ea23525e5e7c14d0449c08e16fac
DipanshuShora <deepanshushora@gmail.com>
2019-08-11 19:02:22 +0530

    defining variables in c

---
+++ definingvariables.c
@@ -0,0 +1,19 @@
+//declaring variables
+#include<stdio.h>
+
+int main(){
+  char ch='A';
+  int n=123;
+  short n1=12;
+  long n2=9868642377;
+  float n3=60.57;
+  double n4=999999999999.1000005;
+  printf("variable %c for character",ch);
+  printf("\nvariable %d for int",n);
+  printf("\nvariable %i for short",n1);
+  printf("\nvairable %ld for long",n2);
+  printf("\nvariable %f for float", n3);
+  printf("\nvariable %lf for double",n4);
+  printf("\n");
+  return 0;
+}
